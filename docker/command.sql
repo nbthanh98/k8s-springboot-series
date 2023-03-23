62c814def77a53022187ffbfb7d33f435ef3de50
thanhnb <thanhnb@unicloud.com.vn>
2023-03-23 11:28:11 +0700

    update

--- docker/command.sql
+++
@@ -1,8 +0,0 @@
-CREATE USER 'students'@'%' IDENTIFIED BY 'VNTechies2023';
-CREATE DATABASE students CHARACTER SET utf8 COLLATE utf8_general_ci;
-GRANT ALL ON students.* TO 'students'@'%';
-
-CREATE USER 'payment'@'%' IDENTIFIED BY 'VNTechies2023';
-CREATE DATABASE payment CHARACTER SET utf8 COLLATE utf8_general_ci;
-GRANT ALL ON payment.* TO 'payment'@'%';
-flush privileges;
